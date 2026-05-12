62775c08823425db58fb7360b096fe1fb1f1f9b4
Nikola Lalicicm <nikolalalicic5@gmail.com>
2026-05-12 23:43:19 +0200

    Auth.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import { http } from "@/lib/http";
 
 export async function register(payload) {
-  const { data } = await http.post("/user/registration", payload);
+  const { data } = await http.post("/user/registration/", payload);
   return data;
 }
 
